0c94c09baa0f96080ab6c788914398f70ef58131
Luiz Picanço <lpicanco@gmail.com>
2016-07-07 17:41:30 -0300

    Nimbus adress adjustment

--- nimbus/start-supervisor.sh
+++ nimbus/start-supervisor.sh
@@ -1,7 +1,10 @@
-export IP=`hostname -i`
-sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
+if [ -z "$NIMBUS_PORT_6627_TCP_ADDR" ]; then
+  export NIMBUS_PORT_6627_TCP_ADDR=`hostname -i`;
+fi
 
-echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+
+echo "storm.local.hostname: $NIMBUS_PORT_6627_TCP_ADDR" >> $STORM_HOME/conf/storm.yaml
 
 /usr/sbin/sshd && supervisord
